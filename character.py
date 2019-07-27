310ea3c44a239cd1a94f0e27e7f7321c771d4130
= <=>
2019-07-27 18:47:21 +0100

    This code is better laid out and I've created space to refactor the button code

--- character.py
+++ character.py
@@ -1,28 +1,27 @@
 """This module is about modeling the character and maybe defining the functions"""
 import tkinter as tk
+
 """I'm gonna start with the character"""
+
+
 class Character:
     """An attempt to model a charachter"""
+
     def __init__(self, name):
         """initialize the attributes of the character"""
         self.name = name
-        self.attributes = {'Strength':1, 'Smarts': 1, 'Dexterity':1, 'Wisdom':1, 'Charisma':1, 'Spirit':1}
-
+        self.attributes = {}
 
     def increase_attribute(self, var):
         """This is to set the attribute back to 1"""
         if self.attributes[var.title()].get() != 6:
-            self.attributes[var.title()].set(self.attributes[var.title()].get() + 1)
-        
+            self.attributes[var.title()].set(
+                self.attributes[var.title()].get() + 1
+            )
 
     def decrease_attribute(self, var):
         """This is to set the attribute back to 1"""
         if self.attributes[var.title()].get() != 0:
-            self.attributes[var.title()].set(self.attributes[var.title()].get() - 1)
-
-
-
-
-
-
-
+            self.attributes[var.title()].set(
+                self.attributes[var.title()].get() - 1
+            )
