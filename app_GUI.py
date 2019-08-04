28b568fab4d9a49a62214b2e0c3baf881421577f
= <=>
2019-08-04 22:52:42 +0100

    blargh

--- app_GUI.py
+++ app_GUI.py
@@ -29,25 +29,6 @@ class GUI(tk.Tk):
 
         self.show_frame("FrontPage")
 
-    def center(toplevel):
-        toplevel.update_idletasks()
-
-        # Tkinter way to find the screen resolution
-        # screen_width = toplevel.winfo_screenwidth()
-        # screen_height = toplevel.winfo_screenheight()
-
-        # PyQt way to find the screen resolution
-        app = QtGui.QApplication([])
-        screen_width = app.desktop().screenGeometry().width()
-        screen_height = app.desktop().screenGeometry().height()
-
-        size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-        x = screen_width/2 - size[0]/2
-        y = screen_height/2 - size[1]/2
-
-        toplevel.geometry("+%d+%d" % (x, y))
-        toplevel.title("Centered!")
-
     def show_frame(self, page_name):
         """Shows the desired frame"""
         frame = self.frames[page_name]
